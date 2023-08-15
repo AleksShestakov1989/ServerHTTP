552c8a6845e3b90f928d69bf3171a891974e3a5c
Aleks <shestakov.aleksey8989@gmail.com>
2023-08-15 19:15:22 +0300

    Add getQueryParams() and getQueryParam(String name)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,34 @@ public class Main {
 
     public static void main(String[] args) {
         Server server = new Server(threadPool);
+        Handler defaultHandler = (request, responseStream) -> {
+            try {
+                final var filePath = Path.of(".", "public", "resources", request.getPath());
+                final var mimeType = Files.probeContentType(filePath);
+
+                final var length = Files.size(filePath);
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, responseStream);
+                responseStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+        server.addHandler("GET", "/app.js", defaultHandler);
+        server.addHandler("GET", "/events.html", defaultHandler);
+        server.addHandler("GET", "/events.js", defaultHandler);
+        server.addHandler("GET", "/forms.html", defaultHandler);
+        server.addHandler("GET", "/index.html", defaultHandler);
+        server.addHandler("GET", "/links.html", defaultHandler);
+        server.addHandler("GET", "/spring.svg", defaultHandler);
+        server.addHandler("GET", "/spring.png", defaultHandler);
+        server.addHandler("GET", "/styles.css", defaultHandler);
 
         server.addHandler("GET", "/classic.html", (request, responseStream) -> {
             try {
@@ -19,7 +47,6 @@ public class Main {
                         "{time}",
                         LocalDateTime.now().toString()
                 ).getBytes();
-                //final String mimeType = Files.probeContentType(filePath);
                 responseStream.write((
                         "HTTP/1.1 200 OK\r\n" +
                                 "Content-Type: " + request.getMethod() + "\r\n" +
@@ -29,7 +56,6 @@ public class Main {
                 ).getBytes());
                 responseStream.write(content);
                 responseStream.flush();
-
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
